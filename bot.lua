dad4d7de1379273c0b9082c9e2880ce295c3fa19
Rob Emanuele <rob@emanuele.us>
2013-11-09 02:20:37 -0800

    Example nick to github map, no long term storage

--- bot.lua
+++ bot.lua
@@ -8,6 +8,7 @@ local string = require('string')
 local irc = require("luvit-irc")
 
 local server
+local nickmap = {}
 local c
 
 --Configure
@@ -31,6 +32,32 @@ if f ~= nil then
 end
 
 
+function split(str, delim, maxNb)
+  -- Eliminate bad cases...
+  if string.find(str, delim) == nil then
+    return { str }
+  end
+  if maxNb == nil or maxNb < 1 then
+    maxNb = 0    -- No limit
+  end
+  local result = {}
+  local pat = "(.-)" .. delim .. "()"
+  local nb = 0
+  local lastPos
+  for part, pos in string.gmatch(str, pat) do
+    nb = nb + 1
+    result[nb] = part
+    lastPos = pos
+    if nb == maxNb then break end
+  end
+  -- Handle the last field
+  if nb ~= maxNb then
+    result[nb + 1] = string.sub(str, lastPos)
+  end
+  return result
+end
+
+
 --Hook Handler
 local function webhook_handler(request, response)
   local postBuffer = ''
@@ -44,32 +71,55 @@ local function webhook_handler(request, response)
     --p("request", request)
     --p("postBuffer", postBuffer)
     ret, error = pcall(function()
-       t = JSON.parse(postBuffer)
-       --p("final", t)
-       --p(t.comment.body)
-       local plus, count
-       plus, count = string.gsub(t.comment.body, ".*([%+%-]%d+).*", "%1")
-       --p("ret", plus, count)
-       if count > 0 then
-         local give = string.format("%s has %s'd %s's %s",
-           t.sender.login,
-           plus,
-           t.issue.user.login,
-	   t.issue.html_url)
-	   p(give)
-	 c:privmsg(config.irc.channel.name, give)
-	end
+      t = JSON.parse(postBuffer)
+      --p("final", t)
+      --p(t.comment.body)
+      local plus, count
+      plus, count = string.gsub(t.comment.body, ".*([%+%-]%d+).*", "%1")
+      --p("ret", plus, count)
+      if count > 0 then
+        local give = string.format("%s has %s'd %s's %s",
+          nickmap[t.sender.login] or t.sender.login,
+          plus,
+          nickmap[t.issue.user.login] or t.issue.user.login,
+          t.issue.html_url)
+          p(give)
+        c:privmsg(config.irc.channel.name, give)
+      end
     end)
   end)
 end
 
+
+local commands = {}
+
+commands["help"] = function(origin, nick, tokens)
+  c:privmsg(origin, "Commands:")
+  c:privmsg(origin, "map <username>; Map a Github username to the current nick")
+  c:privmsg(origin, "about; see: http://github.com/rjemanuele/pullwatcher")
+end
+
+commands["map"] = function(origin, nick, tokens)
+  nickmap[tokens[2]] = nick
+  c:privmsg(origin, string.format("Nick: %s mapped to Github: %s", nick, tokens[2]))
+end
+
+commands["about"] = commands["help"]
 
 --IRC Handler
 local function irc_handler(origin, nick, msg)
   if origin == config.irc.nick then
      origin = nick
   end
-  c:privmsg(origin, "This bot has nothing to say yet, see: http://github.com/rjemanuele/pullwatcher")
+  --trim
+  msg:gsub("^%s+", ""):gsub("%s+$", "")
+  local tokens = split(msg, "%s+")
+  p(tokens)
+  if not tokens or not tokens[1] or not commands[tokens[1]:lower()] then
+    c:privmsg(origin, "Unknown command")
+    tokens[1] = 'help'
+  end
+  commands[tokens[1]:lower()](origin, nick, tokens)
 end
 
 
@@ -85,7 +135,7 @@ c:on("privmsg", function (origin, nick, msg)
   if origin:sub(1,1) ~= '#' then
     irc_handler(origin, nick, msg)
   end
-  local _, i =  msg:gsub("^" .. config.irc.nick .. "[%p ][ ]*", "")
+  msg, i =  msg:gsub("^" .. config.irc.nick .. "[%p ][ ]*", "")
   if i == 1 then
     irc_handler(origin, nick, msg)
   end
