bea59ab1cecf8e3476728dec4ddd5608ebea90c7
Rob Emanuele <rob@emanuele.us>
2014-12-23 19:33:14 -0800

    support multiple irc servers

--- bot.lua
+++ bot.lua
@@ -1,5 +1,7 @@
 #!/usr/bin/env luvit
 
+local table = require('table')
+local Object = require('core').Object
 local http = require("http")
 local io = require("io")
 local JSON = require('json')
@@ -8,7 +10,7 @@ local string = require('string')
 local irc = require("luvit-irc")
 
 local server
-local c
+local m
 
 --Configure
 local argv = require('luvit-options')
@@ -41,8 +43,8 @@ local function webhook_handler(request, response)
     response:write("Hello")
     response:finish()
     local t = ''
-    --p("request", request)
-    --p("postBuffer", postBuffer)
+    p("request", request)
+    p("postBuffer", postBuffer)
     ret, error = pcall(function()
       t = JSON.parse(postBuffer)
       --p("final", t)
@@ -57,56 +59,91 @@ local function webhook_handler(request, response)
             t.issue.user.login,
             t.issue.html_url)
           p(give)
-          c:privmsg(config.irc.channel.name, give)
+          m:sendtoall(give)
         end)
     end)
   end)
 end
 
 
+local BotIrc = irc:extend()
+
 --IRC Handler
-local function irc_handler(origin, nick, msg)
-  if origin == config.irc.nick then
+function BotIrc:irc_handler(origin, nick, msg)
+  if origin == self.conf.nick then
      origin = nick
   end
-  c:privmsg(origin, "This bot has nothing to say yet, see: http://github.com/rjemanuele/pullwatcher")
+  self:privmsg(origin, "This bot has nothing to say yet, see: http://github.com/rjemanuele/pullwatcher")
 end
 
 
---IRC Setup
-c = irc:new()
-c:on("connected", function (x)
-  local chan = config.irc.channel.name
-  if config.irc.channel.password then
-    chan = chan .. " " .. config.irc.channel.password
-  end
-  c:join(chan)
-end)
-c:on("data", function (x)
-  p(":>: "..x)
-end)
-c:on("dataout", function (x)
-  p(":<: "..x)
-end)
-c:on("privmsg", function (origin, nick, msg)
-  if origin:sub(1,1) ~= '#' then
-    irc_handler(origin, nick, msg)
-  end
-  local _, i =  msg:gsub("^" .. config.irc.nick .. "[%p ][ ]*", "")
-  if i == 1 then
-    irc_handler(origin, nick, msg)
-  end
-end)
-c:on("notice", function (orig, msg)
-  if config.irc.nick_pass and msg:find("/msg NickServ identify") then
-    c:privmsg ("NickServ", "identify " .. config.irc.nick_pass)
-  end
-end)
+function BotIrc:initialize(conf)
+  irc.initialize(self)
 
+  self.conf = conf
+
+  local this = self
+
+  self:on("connected", function (x)
+    local chan = conf.channel.name
+    if conf.channel.password then
+      chan = chan .. " " .. conf.channel.password
+    end
+    this:join(chan)
+  end)
+  self:on("data", function (x)
+    p(":>: "..x)
+  end)
+  self:on("dataout", function (x)
+    p(":<: "..x)
+  end)
+  self:on("privmsg", function (origin, nick, msg)
+    if origin:sub(1,1) ~= '#' then
+      this:irc_handler(origin, nick, msg)
+    end
+    local _, i =  msg:gsub("^" .. this.conf.nick .. "[%p ][ ]*", "")
+    if i == 1 then
+      this:irc_handler(origin, nick, msg)
+    end
+  end)
+  self:on("notice", function (orig, msg)
+    if this.conf.nick_pass and msg:find("/msg NickServ identify") then
+      this:privmsg ("NickServ", "identify " .. this.conf.nick_pass)
+    end
+  end)
+end
+
+
+
+local MultiBot = Object:extend()
+
+function MultiBot:initialize(conf)
+  self.ircs = {}
+
+  local i, v
+  for i,v in ipairs(conf) do
+    local b = BotIrc:new(v)
+    table.insert(self.ircs, b)
+    b:connect(v.host, v.port, v.user or v.nick, v.nick, {ssl=v.ssl, sasl_auth=v.sasl_auth})
+  end
+end
+
+function MultiBot:sendtoall(msg)
+  local i, v
+  for i,v in ipairs(self.ircs) do
+    v:privmsg (v.conf.channel.name, msg)
+  end
+end
+
+
+process:on('error', function(err)
+  p('Fatal error', err)
+  process.exit(255)
+end)
 
 --Go
+m = MultiBot:new(config.irc)
 server = http.createServer(webhook_handler)
-c:connect(config.irc.host, config.irc.port, config.irc.user or config.irc.nick, config.irc.nick, {ssl=config.irc.ssl, sasl_auth=config.irc.sasl_auth})
 server:listen(config.http.port, config.http.addr)
 
 print(string.format("Server listening at http://%s:%d/", config.http.addr, config.http.port))
